9d9039988681eca01b4aa4bb14b6b4572221261e
Francisco José C. Silva <franzecs@gmail.com>
2019-05-03 14:41:11 -0300

    Update firebase-messaging-sw.js

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -2,7 +2,7 @@ importScripts('https://www.gstatic.com/firebasejs/5.8.4/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/5.8.4/firebase-messaging.js');
 
 firebase.initializeApp({
-  messagingSenderId: '77247144900'
+  messagingSenderId: '#############'
 });
 
 const messaging = firebase.messaging();
